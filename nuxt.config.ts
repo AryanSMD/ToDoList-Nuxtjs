3e276f4d3833fdddba66dc7184612eecb1cc5267
AryanSMD <aryansmd79@gmail.com>
2024-07-31 10:37:40 +0330

    updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'To Do List'
+    }
+  },
   css: ['assets/css/style.css'],
   postcss: {
     plugins: {
